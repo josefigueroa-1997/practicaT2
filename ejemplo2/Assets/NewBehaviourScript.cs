644eda2da98532b7759bb74ea653bc10c6130b96
josefigueroa-1997 <80905295+josefigueroa-1997@users.noreply.github.com>
2021-03-22 14:15:15 -0300

    inicio de proyecto
    juego basico

---
+++ ejemplo2/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float Speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); 
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3( horizontal, 0.0f,vertical)*Time.deltaTime*Speed);
+    }
+}
